a8eafed5e9236de8ed80114d6fb43f8eb9c7bfd2
ihsankhwaritsmi <alkhwaritsmi@gmail.com>
2025-08-22 09:59:29 +0700

    renamed:    old-code/average_speed_over_time.png -> archive/average_speed_over_time.png 	renamed:    old-code/centroid.py -> archive/centroid.py 	renamed:    old-code/sort.py -> archive/sort.py 	renamed:    video_shorter.mp4 -> assets/video_shorter.mp4 	deleted:    average_speed_over_time.png 	deleted:    fish_positions.csv 	renamed:    best.pt -> models/best.pt 	renamed:    data_handler.py -> src/data_handler.py 	renamed:    main.py -> src/main.py 	new file:   src/result/average_speed_over_time.png 	new file:   src/result/fish_positions.csv 	renamed:    fish_trajectories.png -> src/result/fish_trajectories.png 	renamed:    sort.py -> src/sort.py 	renamed:    utils.py -> src/utils.py 	renamed:    video_processor.py -> src/video_processor.py

--- src/main.py
+++ src/main.py
@@ -5,13 +5,14 @@ from data_handler import save_fish_positions_to_csv, plot_average_speed, plot_fi
 # Constants
 TRACK_TIME_WINDOW = 1.0  # seconds
 SKIP_FRAME = 1
-VIDEO_PATH = "video_shorter.mp4"
-MODEL_PATH = "best.pt"
-OUTPUT_CSV = "fish_positions.csv"
-OUTPUT_GRAPH_SPEED = "average_speed_over_time.png"
-OUTPUT_GRAPH_TRAJECTORY = "fish_trajectories.png"
+VIDEO_PATH = "../assets/video_shorter.mp4"
+MODEL_PATH = "../models/best.pt"
+OUTPUT_CSV = "result/fish_positions.csv"
+OUTPUT_GRAPH_SPEED = "result/average_speed_over_time.png"
+OUTPUT_GRAPH_TRAJECTORY = "result/fish_trajectories.png"
 
 if __name__ == "__main__":
+    os.makedirs('result', exist_ok=True)
     processor = VideoProcessor(
         model_path=MODEL_PATH,
         video_path=VIDEO_PATH,
